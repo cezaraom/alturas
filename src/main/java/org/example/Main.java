22b3eb8b39b341783c4a766600f7652d48410c12
cezaraom <cezaraom88@hotmail.com>
2024-02-19 11:29:51 -0300

    primeiro envio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,58 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Quantas pessoas serao digitadas?");
+        int n = sc.nextInt();
+
+        String[] nomes = new String[n];
+        int[] idades = new int[n];
+        double[] alturas = new double[n];
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Dados da " + (i + 1) + "a pessoa: ");
+            System.out.println("Nome: ");
+            nomes[i] = sc.next();
+            System.out.println("Idade: ");
+            idades[i] = sc.nextInt();
+            System.out.println("Altura: ");
+            alturas[i] = sc.nextDouble();
         }
+
+        double somaAlturas = 0;
+
+        for (int i = 0; i < n; i++) {
+            somaAlturas += alturas[i];
+        }
+        double alturaMedia = somaAlturas / n;
+
+        System.out.printf("Altura média: %.2f%n", alturaMedia);
+
+        int cont = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (idades[i] < 16) {
+                cont++;
+            }
+        }
+        double idadeMenorDeDezesseis = cont * 100 / n;
+
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%% %n", idadeMenorDeDezesseis);
+
+        for (int i = 0; i < n; i++) {
+            if (idades[i] < 16) {
+                System.out.println(nomes[i]);
+            }
+        }
+
     }
 }
